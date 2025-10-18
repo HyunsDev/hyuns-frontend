12096ee399d64d2526e2f9f0400ab1a25939925a
박현우 <hyuns@hyuns.dev>
2025-10-18 22:29:23 +0900

    feat: Panel MaxWidth 추가

--- apps/dash/app/memo/page.tsx
+++ apps/dash/app/memo/page.tsx
@@ -14,30 +14,57 @@ import { WorkbenchPanel } from "@workspace/ui/layouts/workbench/components/Panel
 
 export default function Page() {
   return (
-    <WorkbenchPanel>
-      <WorkbenchPanelHeader>
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem className="hidden md:block">
-              <BreadcrumbLink href="#">All Inboxes</BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator className="hidden md:block" />
-            <BreadcrumbItem>
-              <BreadcrumbPage>Inbox</BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
-      </WorkbenchPanelHeader>
-      <WorkbenchPanelContent>
-        <div className="flex flex-1 flex-col gap-4 p-4">
-          {Array.from({ length: 24 }).map((_, index) => (
-            <div
-              key={index}
-              className="bg-muted/50 aspect-video h-12 w-full rounded-lg"
-            />
-          ))}
-        </div>
-      </WorkbenchPanelContent>
-    </WorkbenchPanel>
+    <>
+      <WorkbenchPanel>
+        <WorkbenchPanelHeader>
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="#">All Inboxes</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Inbox</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </WorkbenchPanelHeader>
+        <WorkbenchPanelContent>
+          <div className="flex flex-1 flex-col gap-4 p-4">
+            {Array.from({ length: 24 }).map((_, index) => (
+              <div
+                key={index}
+                className="bg-muted/50 aspect-video h-12 w-full rounded-lg"
+              />
+            ))}
+          </div>
+        </WorkbenchPanelContent>
+      </WorkbenchPanel>
+      <WorkbenchPanel>
+        <WorkbenchPanelHeader sidebarButtonMode="hide">
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="#">All Inboxes</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Inbox</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </WorkbenchPanelHeader>
+        <WorkbenchPanelContent>
+          <div className="flex flex-1 flex-col gap-4 p-4">
+            {Array.from({ length: 24 }).map((_, index) => (
+              <div
+                key={index}
+                className="bg-muted/50 aspect-video h-12 w-full rounded-lg"
+              />
+            ))}
+          </div>
+        </WorkbenchPanelContent>
+      </WorkbenchPanel>
+    </>
   );
 }
--- packages/ui/src/layouts/workbench/components/Panel/WorkbenchPanel.tsx
+++ packages/ui/src/layouts/workbench/components/Panel/WorkbenchPanel.tsx
@@ -1,8 +1,30 @@
-export function WorkbenchPanel({ children }: { children: React.ReactNode }) {
+import { cn } from "@workspace/ui/lib/utils";
+
+export function WorkbenchPanel({
+  children,
+  className,
+  maxWidth,
+}: {
+  children: React.ReactNode;
+  className?: string;
+  maxWidth?: string;
+}) {
   return (
-    <div className="flex flex-col w-full h-full max-h-[100dvh] bg-workbench-background px-0 pt-0">
+    <div
+      className={cn(
+        "flex flex-col flex-1 w-full h-full max-h-[100dvh] bg-workbench-background px-0 pt-0"
+      )}
+      style={{
+        maxWidth: maxWidth,
+      }}
+    >
       <div className="window-draggable h-[10px] w-full"></div>
-      <div className="relative flex flex-col h-full max-h-[calc(100dvh-20px)] overflow-hidden border rounded-lg shadow-lg bg-background">
+      <div
+        className={cn(
+          "relative flex flex-col h-full max-h-[calc(100dvh-20px)] overflow-hidden border rounded-lg shadow-lg bg-background",
+          className
+        )}
+      >
         {children}
       </div>
     </div>
